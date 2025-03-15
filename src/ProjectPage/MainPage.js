1809cb3f996ef01b69c9bdeb6f36ec2a575a805f
Jai Bisht <jaibisht234@gmail.comgit config --listgit config --global user.email jaibisht234@gmail.com>
2025-03-15 12:06:07 +0530

    new commit

--- src/ProjectPage/MainPage.js
+++ src/ProjectPage/MainPage.js
@@ -3,12 +3,12 @@ import ProjectDetails from "./ProjectDetail";
 import RecentWork from "./RecentWork";
 import BottumPage from "./BottumPage";
 function MainPage() {
-return (
-<>
-<RecentWork />
-<ProjectDetails />
-<BottumPage />
-</>
-);
+    return (
+        <>
+            <RecentWork />
+            <ProjectDetails />
+            <BottumPage />
+        </>
+    );
 }
 export default MainPage;
