adae9b02829ab237954678baa59ba5a48b1f4485
Jai Bisht <jaibisht234@gmail.comgit config --listgit config --global user.email jaibisht234@gmail.com>
2025-03-15 12:04:29 +0530

    new commit

--- src/ProjectPage/BottumPage.js
+++ src/ProjectPage/BottumPage.js
@@ -1,15 +1,15 @@
 import React from "react";
 function BottomPage() {
-return (
-<div>
-<p className="container text-center mt-5">
-I have a good grip in React.js i have build several project <br></br>
-using React. I love working in an agile environment and constantly
-learning new tools and technologies
-<span style={{ color: "skyblue" }}> to improve my skills.</span>
-<br></br>I like to connect with people and I appreciate Team work.
-</p>
-</div>
-);
+    return (
+        <div>
+            <p className="container text-center mt-5" style={{fontSize:"1.1rem" , color:"white"}}>
+                I have a good grip in React.js i have build several project <br></br>
+                using React. I love working in an agile environment and constantly
+                learning new tools and technologies
+                <span style={{ color: "#5DB9EE" }}> to improve my skills.</span>
+                <br></br>I like to connect with people and I appreciate Team work.
+            </p>
+        </div>
+    );
 }
 export default BottomPage;
